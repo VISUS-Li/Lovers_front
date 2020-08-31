a6adf0bea448b2b94b7f88af94e05060325da16e
VISUS-Li <1183989659@qq.com>
2020-08-31 19:33:01 +0800

    日常修改

--- uni_app_lovers/pages/notelist/notelist.vue
+++ uni_app_lovers/pages/notelist/notelist.vue
@@ -1,8 +1,40 @@
 <template>
-	<view>test</view>
+	<view>
+		<nav-bar>事件清单</nav-bar>
+		<ss-navbar :navArr="navArr" :tabCurrentIndex="currentIndex" @navbarTap="navbarTapHandler1">
+		</ss-navbar>
+	</view>
 </template>
 
 <script>
+	import navBar from "../../components/third-common/zhouWei-navBar/index.vue";
+	import ssNavBar from "../../components/third-common/ss-navbar/ss-navbar.vue";
+	export default {
+		components: {
+			navBar,
+			ssNavBar
+		},
+		data() {
+			return {
+				currentIndex: 0,
+
+				navArr: [{
+					title: '待完成',
+					url: ''
+				}, {
+					title: '已完成',
+					url: ''
+				}]
+			}
+		},
+		methods: {
+			navbarTapHandler(index) {
+				this.currentIndex = index;
+				var obj = this.navArr[index];
+			}
+		}
+
+	}
 </script>
 
 <style>
