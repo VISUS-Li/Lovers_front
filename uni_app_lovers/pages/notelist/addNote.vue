f5f8583228701780757321b16757822f4fc62b39
VISUS-Li <1183989659@qq.com>
2020-09-04 00:23:04 +0800

    选择日期

--- uni_app_lovers/pages/notelist/addNote.vue
+++ uni_app_lovers/pages/notelist/addNote.vue
@@ -7,7 +7,7 @@
 		</u-navbar>
 
 
-		<u-form>
+		
 			<view class="formItem">
 				<u-icon class="HeaderIcon" name="bag">
 				</u-icon>
@@ -17,9 +17,16 @@
 							<text class="HeaderText">一起做些什么好呢</text>
 						</view>
 					</view>
-					<u-form-item label-width="120" :border-bottom="true" label-position="left" class="formItemContext">
-						<u-input :border="border" placeholder="事件名称" v-model="model.name" type="text"></u-input>
-					</u-form-item>
+					<u-field
+						placeholder="事件名称"
+						:clearable="false"
+						class="formItemContext"
+						label-width="0"
+						:border-bottom="true"
+						:field-style = "fieldStyle"
+					>
+					</u-field>
+					<u-line class="line"></u-line>
 				</view>
 			</view>
 
@@ -29,15 +36,20 @@
 				<view class="itemRight">
 					<view class="inputHeader">
 						<view class="HeaderTextView">
-							<text class="HeaderText">在这之间搞定它吧</text>
+							<text class="HeaderText">在这之前搞定它吧</text>
 						</view>
 					</view>
-					<u-form-item label-width="120" :border-bottom="true" label-position="left" class="formItemContext">
-						<u-input :border="border" placeholder="事件名称" v-model="model.name" type="text"></u-input>
-					</u-form-item>
+						<view class="dateSection">
+							<view class="dateText">{{CaseDate}}</view>
+							<u-button @click="dateBtnClick" data-name="3333" shape="circle" type="primary" :custom-style="dateBtnStyle">
+								<u-icon name="close" color="#fff"></u-icon>
+							</u-button>
+						</view>
 				</view>
 			</view>
-		</u-form>
+			
+			<u-calendar v-model="dateSelectShow" >
+			</u-calendar>
 	</view>
 </template>
 
@@ -72,30 +84,38 @@
 					"color": "#fff",
 					left: "140rpx"
 				},
-
-				//表单相关参数
-				model: {
-					name: '',
-					sex: '',
-					likeFruit: '',
-					intro: '',
-					payType: '支付宝',
-					agreement: false,
-					region: '',
-					goodsType: '',
-					phone: '',
-					code: '',
-					password: '',
-					rePassword: '',
-					remember: false,
-					photo: ''
+				
+				//输入框自定义样式
+				fieldStyle:{
 				},
-				formLabelStyle: {
-					"background-color": "red",
-					color: "white",
-
+				CaseDate:"",
+				
+				dateBtnStyle:{
+					width:"100rpx",
+					height:"50rpx",
+				},
+				dateSelectShow:false
+			}
+		},
+		
+		computed:{
+			selectedDate:{
+				get:function(){
+					
+				},
+				set:function(newDate){
+					this.CaseDate = newDate;
 				}
 			}
+		},
+		
+		methods:{
+			dateBtnClick:function(){
+				this.dateSelectShow = true;
+			}
+		},
+		onLoad() {
+			this.CaseDate = new Date().toISOString().slice(0, 10);
 		}
 	}
 </script>
@@ -103,9 +123,14 @@
 <style>
 	.formItem {
 		display: flex;
+		margin-top: 30rpx;
 	}
-
-	.itemRight {}
+	.line{
+	}
+	
+	.itemRight {
+		width: 100%;
+	}
 
 	.inputHeader {
 		margin-top: 20rpx;
@@ -118,10 +143,10 @@
 		margin-left: 30rpx;
 		margin-top: 20rpx;
 		margin-right: 10rpx;
-		background-color: #EC971F;
+		background-color: "";
 		padding-left: 5rpx;
 		padding-right: 7rpx;
-		color: #FFFFFF;
+		color: #EC971F;
 	}
 
 	.inputHeader .HeaderTextView {
@@ -145,4 +170,16 @@
 	.formItemContext {
 		margin-left: 10rpx;
 	}
+	
+	.dateSection{
+		width: 850rpx;
+		height: 50rpx;
+		padding-left: 30rpx;
+		padding-top: 10rpx;
+		margin: 0;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
 </style>
